eaddab2610373e05549ccaec150c833a669725e3
Michael Isasi <Jetmax25@gmail.com>
2017-05-04 18:25:04 -0400

    UI put in place

---
+++ StanfordCalculator/CalculatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalculatorViewController.swift
+//  StanfordCalculator
+//
+//  Created by Anna Stavropoulos on 5/4/17.
+//  Copyright © 2017 Jetmax. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
